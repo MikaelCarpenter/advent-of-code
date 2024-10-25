07cb94600bbda014fd3a753ab4c7b8990e822600
Mikael Carpenter <mikaelcarp.dev@gmail.com>
2024-10-25 08:55:27 -0700

    complete 2015/001 in rust

--- rust/twenty_fifteen/src/bin/001.rs
+++ rust/twenty_fifteen/src/bin/001.rs
@@ -1,3 +1,41 @@
+const INPUT: &str = include_str!("001.input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let destination_floor = get_destination_floor(INPUT);
+    println!("Destination Floor: {}", destination_floor);
+
+    let basement_position = get_basement_position(INPUT);
+    println!("First position in basement: {}", basement_position);
+}
+
+fn get_destination_floor(input: &str) -> i32 {
+    let mut floor = 0;
+
+    for char in input.chars() {
+        match char {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => eprintln!("Invalid character: {}", char),
+        }
+    }
+
+    return floor;
+}
+
+fn get_basement_position(input: &str) -> i32 {
+    let mut floor = 0;
+
+    for (position, char) in input.chars().enumerate() {
+        match char {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => eprintln!("Invalid character: {}", char),
+        }
+
+        if floor < 0 {
+            return position as i32 + 1;
+        }
+    }
+
+    return -1;
 }
